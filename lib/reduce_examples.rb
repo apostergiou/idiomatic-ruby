f47d686fecf134223a37f24440b44c9f2a050e6d
Apostolis Stergiou <apostergiou@gmail.com>
2017-05-03 21:54:43 +0300

    Use reduce to compute factorials

--- lib/reduce_examples.rb
+++ lib/reduce_examples.rb
@@ -1,6 +1,6 @@
-<<~DOC
-  Use reduce to get the sum of an array
-DOC
-
+# Use reduce to get the sum of an array
 a = [1, 2, 3]
 a.reduce :+ # => 6
+
+# Use reduce to get the factorial of a number
+(1..n).reduce(1, :*)
